6fd836127667d996c6371a077a9474046bab9ec5
Paul Laskowski <paul@warmbowski.org>
2023-12-16 15:33:06 -0800

    remove unused

--- src/day06/data.ts
+++ src/day06/data.ts
@@ -4,14 +4,6 @@ Distance:  9  40  200
 `
   .trim()
   .split("\n");
-// should equal value for part 1
-
-export const testData2 = `
-data
-`
-  .trim()
-  .split("\n");
-// should equal value for part 2
 
 export const data = `
 Time:        56     71     79     99
