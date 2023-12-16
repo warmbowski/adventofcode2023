00b68d874b8bd9d13e3ece2b73ef74b3f7eb3318
Paul Laskowski <paul@warmbowski.org>
2023-12-16 15:41:07 -0800

    fix template

--- src/template/data.ts
+++ src/template/data.ts
@@ -3,14 +3,12 @@ data
 `
   .trim()
   .split("\n");
-// should equal value for part 1
 
 export const testData2 = `
 data
 `
   .trim()
   .split("\n");
-// should equal value for part 2
 
 export const data = `
 data
